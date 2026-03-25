d26a1b3a45af30d70a9da64eb07289ce29ccfffa
Meli <ms.melcastro@gmail.com>
2026-03-25 14:04:16 -0700

    fixed errors

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const $ = (id) => document.getElementById(id);
+
+    $("empForm").addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        console.log("Employee ID:", $("id").value);
+        console.log("Name:", $("name").value);
+        console.log("Extension:", $("ext").value);
+        console.log("Email:", $("email").value);
+        console.log("Department:", $("department").value);
+    });
+});
